011a9b55a6962718a58772fba85bcabfbe807edd
Rajkumar M <rajkumar.mp@zohocorp.com>
2024-11-11 18:32:24 +0530

    Add the env files

--- enviroment.ts
+++ enviroment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    // server:"http://localhost:8080"
-    server:"http://54.234.238.77:8080" 
+    // server:"http://localhost:8080",
+    production: false,
+    server:"http://54.234.238.77:8080"
   };
